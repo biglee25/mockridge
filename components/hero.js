f6d956c79e7331352b670f46b313b2ebcd324c29
Lee Attwater <leeattwater@gmail.com>
2020-10-10 15:17:53 +0100

    hero

--- components/hero.js
+++ components/hero.js
@@ -2,6 +2,15 @@ import styled from "styled-components";
 
 const Container = styled.section`
   text-align: center;
+  background-image: url("images/hero.jpg");
+  background-position: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
 `;
 
 export default function Hero() {
--- components/navbar.js
+++ components/navbar.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 
 import styled from "styled-components";
-import { Fade as Hamburger } from "hamburger-react";
+import { Twirl as Hamburger } from "hamburger-react";
 
 const Navbar = styled.div`
+postion: fixed
   height: auto;
   top: 0;
   left: 0;
-  background: black;
+  background: #333;
   color: white;
   padding: 2rem;
   text-align: center;
@@ -19,6 +20,7 @@ const Navbar = styled.div`
 
 const Menu = styled.div`
   position: absolute;
+  visibility: hidden;
   top: 0;
   right: 0;
   min-width: 100%;
@@ -32,7 +34,7 @@ const Menu = styled.div`
   visibility: ${({ nav }) => (nav ? "visible" : "hidden")};
   opacity: ${({ nav }) => (nav ? 1 : 0)};
 
-  transition: all 1s ease-in-out;
+  transition: all 0.2s ease-in-out;
   z-index: 2;
 
   ul {
@@ -42,9 +44,7 @@ const Menu = styled.div`
     li {
       text-decoration: none;
       list-style: none;
-      font-family: Anton;
-      font-size: 2rem;
-      margin-top: 2rem;
+      margin-top: 0.5rem;
     }
   }
 `;
@@ -62,8 +62,8 @@ export default function Nav() {
   return (
     <div>
       <Navbar>
-        {/*         <img src={logo} width="50%" alt="logo"></img>
-         */}{" "}
+        <img src="images/logo.svg" width="20%" alt="logo"></img>
+
         <a onClick={() => showNav(!nav)}>
           <Icon>
             <Hamburger direction="right" color="#fff" />
