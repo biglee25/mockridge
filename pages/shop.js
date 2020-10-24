d434a5c0433a82ffa67a4d410268fc832dfa96c9
Lee Attwater <leeattwater@gmail.com>
2020-10-24 20:59:12 +0100

    new2

--- pages/shop.js
+++ pages/shop.js
@@ -1,16 +1,11 @@
 import styled from "styled-components";
 import Navbar from "../components/navbar";
 
-const frame = styled.div`
-iframe{
-	display:block;
-	margin:0 auto;
-}
-`
+
 
 function iframe() {
     return {
-        __html: '<embed align="center" src="shop.html" style="position: absolute; top: 100px; right: 0; bottom: 0; padding: 0; margin: 0 auto; min-width: 100%; height: 100%; border: none; overflow-x: hidden"></embed>'
+        __html: '<embed align="center" src="shop.html" style="position: absolute; top: 100px; left: 0 right: 0;  margin: 0 auto; padding: 0; width: 100%; border: none; overflow-x: hidden;></embed>'
     }
 }
 
@@ -18,8 +13,8 @@ function iframe() {
 export default function Shop() {
     return (
         <>
-            <Navbar/>
-            <div dangerouslySetInnerHTML={iframe()} className="frame" />
+        <Navbar/>
+            <div dangerouslySetInnerHTML={iframe()}/>
         </>
 
         )
