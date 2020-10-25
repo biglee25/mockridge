4da6d94935bfd7b94efb33a1836b71b6236c03fa
Lee Attwater <leeattwater@gmail.com>
2020-10-25 10:52:15 +0000

    final?

--- pages/shop.js
+++ pages/shop.js
@@ -4,7 +4,7 @@ import Navbar from "../components/navbar";
 
 function iframe() {
     return {
-        __html: '<embed align="center" src="shop.html" style="position: absolute; top: 100px; right: 0; bottom: 0; padding: 0; margin: 0 auto; width: 100%; height: 100%; border: none; overflow-x: hidden; overflow-y: scroll; pointer-events: auto;"></embed>'
+        __html: '<embed align="center" src="shop.html" style="position: absolute; top: 80px; right: 0; bottom: 0; padding: 0; margin: 0 auto; width: 100%; height: 100%; border: none; overflow-x: hidden; overflow-y: scroll; pointer-events: auto;"></embed>'
     }
 }
 
